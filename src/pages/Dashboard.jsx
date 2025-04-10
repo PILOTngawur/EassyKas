99a5d476fc5a29aa42544c28279e883f55ac50a1
Erlangga <141829468+PILOTngawur@users.noreply.github.com>
2025-04-10 22:27:54 +0700

    Update Slidebar and position page.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './style/Dashboard.css'
+
 
 function Dashboard() {
   return (
-    <div>dashboard pages</div>
+    <div className="dashboard">
+      <div className="saldo-box">
+        <div className="label">Saldo Tersedia</div>
+        <div className="amount"></div>
+      </div>
+      <div className="box-container">
+        <div className="box pemasukan">
+          <div className="title">Pemasukan</div>
+          
+        </div>
+        <div className="box pengeluaran">
+          <div className="title">Pengeluaran</div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
