5919dceb2d4e342ee1a5cde423b69c87be863b01
Fiku24 <webhosting1124@gmail.com>
2025-04-15 21:23:43 +0700

    Perubahan firdaus

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Pengeluarankas from './pages/Pengeluarankas'
 import Laporan from './pages/Laporan'
 import Dashboard from './pages/dashboard'
 import "bootstrap"
+import CreatSiswa from './pages/TabSiswa/CreatSiswa'
+import UpdateSiswa from './pages/TabSiswa/UpdateSiswa'
 
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
         <Route path='/' element={<Dashboard/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path='/tabsiswa' element={<TabSiswa/>}/>
+        <Route path='/tabsiswa/create' element={<CreatSiswa/>}/>
+        <Route path='/tabsiswa/update/:id' element={<UpdateSiswa/>}/>
         <Route path='/catatkas' element={<Catatkas/>}/>
         <Route path='/pengeluarankas' element={<Pengeluarankas/>}/>
         <Route path='/laporan' element={<Laporan/>}/>
